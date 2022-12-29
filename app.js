e81725e30ec6768edbcce0163355d8de3270517c
aditi walia <waliaaditi93@gmail.com>
2022-12-29 15:55:07 +0530

    changes done

--- app.js
+++ app.js
@@ -29,3 +29,12 @@ function clickHandler() {
 };
 
 btnTranslate.addEventListener("click", clickHandler)
+
+        <footer class="footer">
+        <div 
+        class="footer-header">About
+        Are you a fan of minions? Did you know that the gibberish they say is an actual language. Use the translator to convert your text from English to Minion speak or Banana language.
+        </div>
+
+
+       
